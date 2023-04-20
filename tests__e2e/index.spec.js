3c9788e3493dd3e834079eeda2cb6304c18c2abb
Béatrice <beatrice.briand@viacesi.fr>
2023-04-20 11:57:38 +0200

    change test

--- tests__e2e/index.spec.js
+++ tests__e2e/index.spec.js
@@ -1,11 +1,25 @@
 const { test, expect } = require("@playwright/test");
 
-test.describe("basic test", async () => {
+/* test.describe("basic test", async () => {
 
   test("if pong is displayed", async ({ page }) => {
-    await page.goto("http://localhost:3000/ping");
+    await page.goto("https://web-goodfood-dev-v1.azurewebsites.net/ping");
 
     const pong = await page.$$("body pre");
     expect(pong).toBeTruthy();
   });
-});
+}); */
+
+test.describe("navigation", () => {
+	test.beforeEach(async ({ page }) => {
+	  // Go to the starting url before each test.
+	  await page.goto("https://web-goodfood-dev-v1.azurewebsites.net/ping");
+	});
+  
+	test("main navigation", async ({ page }) => {
+	  // Assertions use the expect API.
+	  await expect(page).toHaveURL("https://web-goodfood-dev-v1.azurewebsites.net/ping");
+	});
+  });
+
+  
